16f3d4d8452d0781f00a2849b8f59075ffc5107e
Khedekar <amit.khedekar@ingrammicro.com>
2025-09-14 18:21:50 +0530

    Flask App Added

--- src/utils.py
+++ src/utils.py
@@ -51,4 +51,14 @@ def evaluate_models(X_train , Y_train, X_test, Y_test, models, param):
     except Exception as e:
         raise CustomException(e , sys)
     
+###############################################################################################
 
+def load_object(file_path):
+    '''
+        load pickle file
+    '''
+    try:
+        with open(file_path, "rb") as file_obj:
+            return dill.load(file_obj)
+    except Exception as e :
+        raise CustomException(e , sys)
